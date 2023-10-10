5a42bbd295c15f090698b7bfbc5953e0767b5012
SCooldude <sammanpandey7@gmail.com>
2023-10-10 13:15:39 -0400

    Account almost done

--- src/banking/Checking.java
+++ src/banking/Checking.java
@@ -1,18 +1,24 @@
 package banking;
 
 public class Checking extends Account{
+
     @Override
     public double monthlyInterest() {
-        return 0;
+        return (balance*.01)/12.0;
     }
 
     @Override
     public double monthlyFee() {
-        return 0;
+        if (balance >= 1000.0) {
+            return 0;
+
+        } else {
+            return 12.0;
+        }
     }
 
     @Override
     public int compareTo(Account o) {
-        return 0;
+        return Double.compare(this.balance, o.balance);
     }
 }
--- src/banking/MoneyMarket.java
+++ src/banking/MoneyMarket.java
@@ -2,4 +2,40 @@ package banking;
 
 public class MoneyMarket extends Savings{
     private int withdrawal; //number of withdrawals
+
+    private double withdrawalFee() {// checks if withdrawlfee applies
+        if (withdrawal > 3.0) {
+            return 10.0;
+        }
+
+        return 0;
+    }
+    public boolean loyaltyStatus(){
+        if (balance <2000.0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    @Override
+    public double monthlyFee() {
+        if (balance>=2000.0){
+            return 0;
+        }
+        else{
+            return 25.0;
+        }
+    }
+    @Override
+    public double monthlyInterest() {
+        if (loyaltyStatus()){
+            return (balance*4.75)/12.0;
+
+        }
+        else{
+            return (balance*4.5)/12.0;
+        }
+    }
+
 }
