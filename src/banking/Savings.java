8ab77dbc9ce50d7958d8a84370e2ef40050da9bf
Fraidoon Pourooshasb <fpourooshasb@gmail.com>
2023-10-12 12:53:53 -0400

    m

--- src/banking/Savings.java
+++ src/banking/Savings.java
@@ -11,7 +11,7 @@ public class Savings extends Account {
     public String accountType() {
         return "Savings";
     }
-    //hello
+    //hello//screw you
 
     @Override
     public double monthlyInterest() {
